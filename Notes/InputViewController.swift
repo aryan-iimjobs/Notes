03eb0857190b74ed9fba7b39715638ee3232576a
Aryan-iimjobs.com <aryan@iimjjobs.com>
2020-02-28 19:16:06 +0530

    added selection, addition, deletion and editing of notes

--- Notes/InputViewController.swift
+++ Notes/InputViewController.swift
@@ -11,21 +11,69 @@ import CoreData
 
 class InputViewController: UIViewController {
     
-    var segueDataTextBoxNote: String?
+    var segueDataNSObject: NSManagedObject?
     
     var managedObjectContext: NSManagedObjectContext?
    
-    @IBOutlet var textFieldTitle: UIView!
+    
+    @IBOutlet weak var textFieldTitle: UITextField!
     
     @IBOutlet weak var textBoxNote: UITextView!
     
-    @IBAction func btnSaveNote(_ sender: UIStoryboardSegue) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Get context reference from AppDelegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        managedObjectContext = appDelegate.persistentContainer.viewContext
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Put focus on textBox
+        textBoxNote.becomeFirstResponder()
+        
+        // Set initial values if note selected from tableView
+        if let note = segueDataNSObject {
+            textBoxNote.text = note.value(forKeyPath: "comment") as? String
+            textFieldTitle.text = note.value(forKeyPath: "title") as? String
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // if note selected from tableView check for data change
+        if let note = segueDataNSObject {
+            let title = note.value(forKeyPath: "title") as? String
+            let note = note.value(forKeyPath: "comment") as? String
+            
+            if note != textBoxNote.text && title != textFieldTitle.text && (!textBoxNote.text!.isEmpty || !textFieldTitle.text!.isEmpty) {
+                saveNote()
+                print("Note Saved old")
+            }else {
+                print("Note not Saved")
+            }
+        } else {
+            if !textBoxNote.text!.isEmpty || !textFieldTitle.text!.isEmpty {
+                saveNote()
+                print("Note Saved new")
+            }
+        }
+    }
+    
+    func saveNote() {
         let entity = NSEntityDescription.entity(forEntityName: "Notes", in: managedObjectContext!)!
 
         let person = NSManagedObject(entity: entity, insertInto: managedObjectContext!)
-
-        person.setValue(textBoxNote.text, forKeyPath: "note")
-        person.setValue(Date().toString(dateFormat: "dd-MM-YYYY HH:mm:ss"), forKeyPath: "date")
+        
+        person.setValue(textFieldTitle.text, forKey: "title")
+        person.setValue(textBoxNote.text, forKeyPath: "comment")
+        person.setValue(Date().toString(dateFormat: "HH:mm:ss dd-MM-YYYY"), forKeyPath: "date")
 
         do {
             try managedObjectContext?.save()
@@ -33,24 +81,7 @@ class InputViewController: UIViewController {
             print("Could not save. \(error), \(error.userInfo)")
         }
         self.navigationController?.popViewController(animated: true)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let text = segueDataTextBoxNote {
-            textBoxNote.text = text
-        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        managedObjectContext = appDelegate.persistentContainer.viewContext
+
     }
 }
 
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -15,9 +15,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var buttonAdd: FloatingButton!
    
-    @IBAction func buttonAddClick(_ sender: Any) {
+    @IBOutlet weak var buttonCancel: FloatingButton!
+    
+    @IBAction func buttonCancelClick(_ sender: Any) {
+        self.tableView.isEditing = false
+        self.tableView.allowsMultipleSelectionDuringEditing = false
         
+        let smallConfiguration = UIImage.SymbolConfiguration(scale: .medium)
+        let smallTrashImage = UIImage(systemName: "plus", withConfiguration: smallConfiguration)
+        buttonAdd.setImage(smallTrashImage, for: .normal)
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.buttonCancel.isEnabled = false
+            self.buttonCancel.alpha = 0
+        })
     }
+    
+    @IBAction func buttonAddClick(_ sender: Any) {}
     
     var managedObjectContext: NSManagedObjectContext?
     
@@ -28,6 +42,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         title = "Notes"
         
+        // Handle long Press on tableView cell
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        self.tableView.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc func handleLongPress(longPressGesture: UILongPressGestureRecognizer) {
+        let p = longPressGesture.location(in: self.tableView)
+        let indexPath = self.tableView.indexPathForRow(at: p)
+        if indexPath == nil {
+            print("Long press on table view, not row.")
+        } else if longPressGesture.state == UIGestureRecognizer.State.began {
+            print("Long press on row, at \(indexPath!.row)")
+            self.tableView.isEditing = true
+            self.tableView.allowsMultipleSelectionDuringEditing = true
+            tableView.reloadData()
+            
+            let smallConfiguration = UIImage.SymbolConfiguration(scale: .medium)
+            let smallTrashImage = UIImage(systemName: "trash", withConfiguration: smallConfiguration)
+            buttonAdd.setImage(smallTrashImage, for: .normal)
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                self.buttonCancel.isEnabled = true
+                self.buttonCancel.alpha = 1
+            })
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,6 +79,8 @@ class ViewController: UIViewController {
         managedObjectContext = appDelegate.persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Notes")
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        fetchRequest.sortDescriptors = [sort]
         
         do {
             arrayAddedNotes = try managedObjectContext!.fetch(fetchRequest)
@@ -54,12 +95,37 @@ class ViewController: UIViewController {
         if segue.identifier == "segueShowNote" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 let controller = segue.destination as! InputViewController
-                let text = arrayAddedNotes[indexPath.row].value(forKeyPath: "note") as? String
-                controller.segueDataTextBoxNote = text
+                
+                let note = arrayAddedNotes[indexPath.row]
+                controller.segueDataNSObject = note
             }
         }
     }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if self.tableView.isEditing {
+            if identifier == "segueInputNote" {
+                if let arrayNote = self.tableView.indexPathsForSelectedRows {
+                    
+                    for note in arrayNote {
+                        self.managedObjectContext?.delete(arrayAddedNotes[note.row])
+                        self.arrayAddedNotes.remove(at: note.row)
+                        self.tableView.deleteRows(at: [note], with: .fade)
+                    }
+            
+                    do {
+                        try self.managedObjectContext?.save()
+                    } catch let error as NSError {
+                        print("Could not save. \(error), \(error.userInfo)")
+                    }
+                }
+            }
+            return false
+        }
+        return true
+    }
 }
+
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
@@ -72,8 +138,35 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         
         let note = arrayAddedNotes[indexPath.row]
         
-        cell.textLabel?.text = note.value(forKeyPath: "note") as? String
+        if note.value(forKeyPath: "title") as? String == nil || note.value(forKeyPath: "title") as? String == "" {
+            cell.textLabel?.text = note.value(forKeyPath: "comment") as? String
+        }
+        else{
+            cell.textLabel?.text = note.value(forKeyPath: "title") as? String
+        }
         cell.detailTextLabel?.text = note.value(forKeyPath: "date") as? String
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let note = arrayAddedNotes[indexPath.row]
+            managedObjectContext?.delete(note)
+            arrayAddedNotes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+
+            do {
+                try managedObjectContext?.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select row \(indexPath.row)")
+    }
+    
 }
